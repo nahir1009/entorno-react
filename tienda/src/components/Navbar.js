7846513d89578094182b5c5789ffca7832d473c8
nahir1009 <nahir1009@gmail.com>
2022-05-16 16:01:41 -0300

    navbar

---
+++ tienda/src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return(
+        <div className="Navbar">
+        <ul>
+            <li>inicio</li>
+            <li>ropa</li>
+            <li>destacado</li>
+            <li>nuevo</li>
+        </ul>
+        </div>
+    );
+}
+
+export default Navbar;
