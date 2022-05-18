5967a498506943eb0ed06239f0bf2171d0020681
nahir1009 <nahir1009@gmail.com>
2022-05-18 14:23:05 -0300

    componentes

---
+++ tienda/src/components/CartWidget.js
@@ -0,0 +1,12 @@
+import Badge from '@material-ui/core/Badge';
+
+const CartWidget = () => {
+    return(
+        <Badge badgeContent={4} 
+        color="primary">
+          <MailIcon />
+        </Badge>
+    )
+    
+
+}
